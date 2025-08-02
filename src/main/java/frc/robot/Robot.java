764731f1f1f49145f7ee43628798bf4a9680c017
Alan Longcoy <ajlongcoy21@gmail.com>
2025-08-02 14:25:45 -0400

    added the setup code for the command scheduler to function properly.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,27 +4,31 @@
 
 package frc.robot;
 
+// Import External Libraries
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
+
+// Import Custom Libraries
 import frc.robot.Actors.Motor;
 import frc.robot.Utils.MotorType;
 import frc.robot.Utils.RotationDir;
 
 /**
  * The methods in this class are called automatically corresponding to each
- * mode, as described in
- * the TimedRobot documentation. If you change the name of this class or the
- * package after creating
- * this project, you must also update the Main.java file in the project.
+ * mode, as described in the TimedRobot documentation. If you change the name of this class or the
+ * package after creating this project, you must also update the Main.java file in the project.
  */
 public class Robot extends TimedRobot {
   public static Motor motor;
   public static XboxController driverController;
 
+  private Command autonomousCommand;
+
   /**
    * This function is run when the robot is first started up and should be used
-   * for any
-   * initialization code.
+   * for any initialization code.
    */
   public Robot() {
     driverController = new XboxController(0);
@@ -36,10 +40,23 @@ public class Robot extends TimedRobot {
 
   @Override
   public void robotPeriodic() {
+    // Runs the Scheduler.  This is responsible for polling buttons, adding newly-scheduled
+    // commands, running already-scheduled commands, removing finished or interrupted commands,
+    // and running subsystem periodic() methods.  This must be called from the robot's periodic
+    // block in order for anything in the Command-based framework to work.
+    CommandScheduler.getInstance().run();
   }
 
   @Override
   public void autonomousInit() {
+    // TODO: Setup the configuration and selection of the autonomous command
+    // TODO: Uncomment the line below for the setup and put in the proper code to make it function correctly.
+    // autonomousCommand = m_robotContainer.getAutonomousCommand();
+
+    // schedule the autonomous command (example)
+    if (autonomousCommand != null) {
+      CommandScheduler.getInstance().schedule(autonomousCommand);
+    }
   }
 
   @Override
@@ -48,6 +65,13 @@ public class Robot extends TimedRobot {
 
   @Override
   public void teleopInit() {
+    // This makes sure that the autonomous stops running when
+    // teleop starts running. If you want the autonomous to
+    // continue until interrupted by another command, remove
+    // this line or comment it out.
+    if (autonomousCommand != null) {
+      autonomousCommand.cancel();
+    }
   }
 
   @Override
