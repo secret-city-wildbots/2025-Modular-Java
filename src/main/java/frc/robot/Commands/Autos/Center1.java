236331cf2bac48bb897acce3dba9c6403f36f71c
Jasper Springer <jasperwspringer@gmail.com>
2025-09-30 18:35:16 -0400

    Tested code from roborodeo. Added some small features and bugfixes to the auto and control systems.

--- src/main/java/frc/robot/Commands/Autos/Center1.java
+++ src/main/java/frc/robot/Commands/Autos/Center1.java
@@ -1,5 +1,6 @@
 package frc.robot.Commands.Autos;
 
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 // Import WPILib Command Libraries
 import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
@@ -8,6 +9,7 @@ import frc.robot.Actors.Subsystems.Drivetrain;
 import frc.robot.Actors.Subsystems.Intake;
 import frc.robot.Actors.Subsystems.Pivot;
 import frc.robot.Commands.Drivetrain.SimpleDrive;
+import frc.robot.Commands.Intake.IntakeAlgaeCommand;
 import frc.robot.Commands.Intake.OuttakeCoralCommand;
 import frc.robot.Commands.pivot.PivotToPositionCommand;
 
@@ -16,22 +18,34 @@ public class Center1 extends SequentialCommandGroup {
     public Center1(Drivetrain drivetrain, Intake intake, Pivot pivot) {
         System.out.println("IN AUTO");
         // Reset IMU
-        drivetrain.resetIMU();
+        //drivetrain.resetIMU();
         // Assign the variables and add the subsystem as a requirement to the command
         addRequirements(drivetrain, intake, pivot);
         addCommands(
             new ParallelRaceGroup(
                 new PivotToPositionCommand(pivot, 140.0),
-                new WaitCommand(5.0)
+                new WaitCommand(2.0)
             ),
-            
-            new SimpleDrive(drivetrain, 0, -1, 0, 2),
+
+            new SimpleDrive(drivetrain, 0, -0.5, 0, 0.75),
+
+            new SimpleDrive(drivetrain, 0, 0.3, 0, 2),
 
             new SimpleDrive(drivetrain, 0, 0, 0, 3),
 
             new ParallelRaceGroup(
                 new OuttakeCoralCommand(intake),
                 new WaitCommand(1)
+            ),
+
+            new ParallelRaceGroup(
+                new IntakeAlgaeCommand(intake),
+                new WaitCommand(1)
+            ),
+
+            new ParallelCommandGroup(
+                 new SimpleDrive(drivetrain, 0, -0.1, 0, 0.5),
+                 new IntakeAlgaeCommand(intake)
             )
         );
     }
